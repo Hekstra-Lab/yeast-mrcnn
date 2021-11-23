7fa8f83ccf619a21d7d982bd5392fa652202ebe3
John Russell <johncrussell25@gmail.com>
2021-11-23 16:21:54 -0500

    BBBC Kaggle DSB 2018 Dataset

--- yeast-mrcnn/datasets.py
+++ yeast-mrcnn/datasets.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 import glob
 import tifffile as tiff
+import os
 
 class YeaZDataset(torch.utils.data.Dataset):
     def __init__(self, root, transforms):
@@ -65,3 +66,58 @@ class YeaZDataset(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.imgs)
+
+class BBBCDataset(torch.utils.data.Dataset):
+    def __init__(self, root, transforms):
+        self.root = root
+        self.transforms = transforms
+        self.subdirs = os.listdir(root)
+
+    def __getitem__(self, idx):
+        # load images and masks
+        sub_path = os.path.join(self.root, self.subdirs[idx])
+        img_file = os.listdir(os.path.join(sub_path,"images"))[0]
+        mask_files = sorted(os.listdir(os.path.join(sub_path,"masks")))
+        
+        img_path = os.path.join(sub_path,'images',img_file)
+        img = plt.imread(img_path).astype(np.float32)[...,0]
+        img = (img-img.min())/(img.max()-img.min())
+        img = img[None,...]
+        
+        masks = np.array([plt.imread(os.path.join(sub_path, "masks", m)) for m in mask_files])
+        
+        # get bounding box coordinates for each mask
+        num_objs = len(mask_files)
+        boxes = []
+        for i in range(num_objs):
+            pos = np.where(masks[i])
+            xmin = np.min(pos[1])
+            xmax = np.max(pos[1])
+            ymin = np.min(pos[0])
+            ymax = np.max(pos[0])
+            boxes.append([xmin, ymin, xmax, ymax])
+
+        # convert everything into a torch.Tensor
+        img = torch.as_tensor(img)
+        boxes = torch.as_tensor(boxes)
+        # there is only one class
+        labels = torch.ones((num_objs,), dtype=torch.int64)
+        masks = torch.as_tensor(masks, dtype=torch.uint8)
+
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["masks"] = masks
+        target["image_id"] = image_id
+        target["area"] = area
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.subdirs) 
