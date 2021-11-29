6f46cdbb41f2f5c6d7767debf28d5f3263c9b384
John Russell <johncrussell25@gmail.com>
2021-11-29 13:49:58 -0500

    add bash line and update data path

--- yeast_mrcnn/scripts/train_bbbc/train_bbbc.sh
+++ yeast_mrcnn/scripts/train_bbbc/train_bbbc.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 #SBATCH -n 1
-#SBATCH -c 16
-#SBATCH -t 120
+#SBATCH -c 8
+#SBATCH -t 720
 #SBATCH -p seas_gpu_requeue
 #SBATCH --gres=gpu:1
 #SBATCH --mem 48G
@@ -9,4 +10,4 @@
 #SBATCH -e errors.txt
 
 module load cudnn/8.1.0.77_cuda11.2-fasrc01
-python train_bbbc.py /n/holyscratch01/hekstra_lab/russell/bbbc_nuclei ~/microscopy-notebooks/yeast_mrcnn_train cuda
+python train_bbbc.py /n/holyscratch01/hekstra_lab/russell/bbbc_nuclei/train/ ~/microscopy-notebooks/yeast_mrcnn_train cuda
